39d4a4802d69ea966130a2953c9c31d2f138a8b9
cmxrenato <renatoaraujoleal@gmail.com>
2025-05-27 14:11:59 -0300

    criação do botão de excluir horarios no sistema de profissionais

--- sistema.php
+++ sistema.php
@@ -20,8 +20,8 @@ if (!isset($_SESSION['medico_id']) || $_SESSION['loggedin'] !== true) {
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
-  <link rel="stylesheet" type="text/css" href="css/sistema.css" />
+  <link href="css/style.css?v=<?= filemtime('css/style.css') ?>" rel="stylesheet" type="text/css">
+  <link href="css/sistema.css?v=<?= filemtime('css/sistema.css') ?>" rel="stylesheet" type="text/css">
   <script src="js/diasDisponiveis.js?v=1.0.7" defer></script>
 </head>
 <body>

06797b2078ddefb88908b54ab7db76055d6534c8
cmxrenato <renatoaraujoleal@gmail.com>
2025-06-04 17:06:16 -0300

    criação do botão de desmarcar

---
+++ consultasConfirmadasMedico.php
@@ -0,0 +1,48 @@
+
+<?php
+
+
+
+include 'conexao.php';
+
+
+$login = $_SESSION['telefone']; 
+
+
+
+
+$sql = "SELECT id, data_consulta, horario, nome, telefone FROM consultas_marcadas WHERE telefone = ?";
+
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("s", $login);
+$stmt->execute();
+$result = $stmt->get_result(); // <-- usa o resultado da execução preparada
+
+if ($result->num_rows > 0) {
+    echo "<table class='table table-striped table'>
+            <tr>
+                <th>Dia da Consulta</th>
+                <th>Horário</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+                               
+            </tr>";
+
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>" . htmlspecialchars($row["data_consulta"]) . "</td>
+                <td>" . htmlspecialchars($row["horario"]) . "</td>
+                <td> ".htmlspecialchars($row["nome"]) . " </td>
+                <td> ".htmlspecialchars($row["telefone"]) . " </td>
+                               
+              </tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "<div class='senao'>Não há consultas marcadas!</div>";
+}
+
+$stmt->close();
+$conexao->close();
+?>
